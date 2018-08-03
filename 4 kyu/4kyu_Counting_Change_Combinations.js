e0a71c3222215fa0cc64b23d92d57d7ce00b8e17
Anatoly Nizhnikov <anatoly.nizhnikov@codeit.com.ua>
2018-08-03 15:51:57 +0300

    New solutions: added new solution -> 4kyu - Counting Change Combinations

---
+++ 4 kyu/4kyu_Counting_Change_Combinations.js
@@ -0,0 +1,13 @@
+const countChange = function (money, coins, i = 0) {
+    if (money < 0) return 0;
+    if (money === 0) return 1;
+
+    if (i === coins.length && money > 0) {
+        return 0;
+    }
+
+    return countChange(money - coins[i], coins, i) + countChange(money, coins, i + 1);
+}
+
+//Article for solution
+//https://algorithms.tutorialhorizon.com/dynamic-programming-coin-change-problem/
