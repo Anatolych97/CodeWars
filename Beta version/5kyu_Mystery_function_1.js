fb4a70470408a106f576ff4cde905c4ed7cf04c8
Anatoly Nizhnikov <anatoly.nizhnikov@codeit.com.ua>
2018-09-14 11:14:37 +0300

    Beta: deleted old solutions and fix bugs

--- Beta version/5kyu_Mystery_function_1.js
+++ Beta version/5kyu_Mystery_function_1.js
@@ -1,22 +1,25 @@
 //Play with the function mystery(x) in your test cases.
 //When you think you've solved the mystery,
 //implement the following function as your answer
-function solved(str) {
+const solved = function(str) {
     console.log(str);
     let ans = '',
-        mas = str.split(' ');
-    if (mas.length === 1 && str.length % 2 !== 0) {
-        ans += oneWord(str);
+        mas = str.trim().split(' ');
+    if (mas.length === 1) {
+        if(str.length % 2 !== 0) {
+          ans += oneWord(str);
+        } else {
+          ans += wordTransf([str]);
+        }
     } else if (mas.length === 2) {
         ans += wordTransf(mas);
     } else {
-        ans += ' '.repeat(mas.length + 1);
-        ans += mas.join('');
+        ans += ' '.repeat(mas.length - 1);
+        ans += wordTransf(mas);
     }
-
     return ans;
 
-    function oneWord(str) {
+    function oneWord(str, cor) {
         let wordArr = str.split('');
         delete wordArr[Math.ceil(wordArr.length / 2)];
         return wordArr.sort((a, b) => a.localeCompare(b)).join('');
@@ -39,6 +42,7 @@ function solved(str) {
         lwCase.sort((a, b) => a.localeCompare(b));
 
         res += upCase.join('') + lwCase.join('');
+
         return res;
     }
 }
